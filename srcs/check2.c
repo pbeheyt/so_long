aaef6a95622a58eda41615821453627c0241f673
Pierre BEHEYT <pbeheyt@e1r13p1.42.fr>
2022-06-26 10:24:11 +0200

    fix wall millieux de map

--- srcs/check2.c
+++ srcs/check2.c
@@ -6,7 +6,7 @@
 /*   By: pbeheyt <pbeheyt@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/14 01:56:10 by pbeheyt           #+#    #+#             */
-/*   Updated: 2022/06/25 11:57:00 by pbeheyt          ###   ########.fr       */
+/*   Updated: 2022/06/26 10:23:14 by pbeheyt          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ int	is_content(char c, int *exit_count, int *player_count, t_map *map)
 		*exit_count += 1;
 	else if (c == 'P')
 		*player_count += 1;
-	if (c == '0' || c == 'C' || c == 'E' || c == 'P')
+	if (c == '0' || c == '1' || c == 'C' || c == 'E' || c == 'P')
 		return (1);
 	return (0);
 }
